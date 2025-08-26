c8bd8f6bac43b9e713c5223d6aad864df0ddd445
Jommp <jommp.dev@gmail.com>
2025-08-25 21:17:54 -0500

    feat(App.tsx): create menu and consumo sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -6,6 +6,16 @@ function App() {
       <header className="bg-teal-600 p-4">
         <h1 className="text-4xl font-bold">Tip Calculator</h1>
       </header>
+
+      <main className='max-w-7xl mx-auto py-20 grid md:grid-cols-2'>
+        <section>
+          <h2>Menu</h2>
+        </section>
+
+        <section>
+          <h2>Consumo</h2>
+        </section>
+      </main>
     </>
   )
 }
