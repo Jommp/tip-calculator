ad4ecb248e6817ff2e18ec995910a181b70f983e
Jommp <jommp.dev@gmail.com>
2025-08-27 13:39:52 -0500

    feat(Components): create TipForm component

---
+++ src/components/TipForm.tsx
@@ -0,0 +1,35 @@
+import { tipOptions } from "../mocks/db"
+
+import type { TipOption } from "../types"
+
+type TipFormProps = {
+  handleSelectTip: (value : TipOption['value']) => void
+}
+
+export default function TipForm({ handleSelectTip } : TipFormProps) {
+  return (
+    <div>
+      <h3 className="text-2xl font-bold mb-2">
+        Tip
+      </h3>
+
+      <form className="flex flex-col justify-between space-y-1">
+        {
+          tipOptions.map(option => (
+            <div key={option.id}>
+              <label htmlFor={option.id}>{option.label}</label>
+              <input
+                type="radio"
+                id={option.id}
+                name="tips"
+                value={option.value}
+                className="ml-2"
+                onChange={() => handleSelectTip(option.value)}
+              />
+            </div>
+          ))
+        }
+      </form>
+    </div>
+  )
+}
